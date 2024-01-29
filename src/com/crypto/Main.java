95d0c76a44c1634ea130742248aaee186fb39ab4
ArtemGonchar0v <shalenstvo228@gmail.com>
2024-01-29 21:16:44 +0200

    move validations into run()

--- src/com/crypto/Main.java
+++ src/com/crypto/Main.java
@@ -9,15 +9,13 @@ import java.io.File;
 import static com.crypto.util.ImputValidation.*;
 
 public class Main {
-    private static final int COMMAND_ARGUMENT_POSITION = 0;
-    private static final int FILE_PATH_ARGUMENT_POSITION = 1;
-    private static final int KEY_ARGUMENT_POSITION = 2;
+    public static final int COMMAND_ARGUMENT_POSITION = 0;
+    public static final int FILE_PATH_ARGUMENT_POSITION = 1;
+    public static final int KEY_ARGUMENT_POSITION = 2;
 
     public static void main(String[] args) {
-        validateInput(args);
-        validateCommand(args[COMMAND_ARGUMENT_POSITION]);
-        validateFilePath(args[FILE_PATH_ARGUMENT_POSITION]);
-        validateKey(args[KEY_ARGUMENT_POSITION]);
+
         Run.run(args);
+
     }
 }
