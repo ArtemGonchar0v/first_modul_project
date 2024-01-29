7764daf96ba8fa86d362c9e9337c6cb2e2290bf7
ArtemGonchar0v <shalenstvo228@gmail.com>
2024-01-29 21:17:31 +0200

    add validations in run()

--- src/com/crypto/util/Run.java
+++ src/com/crypto/util/Run.java
@@ -6,13 +6,21 @@ import com.crypto.service.EncryptService;
 
 import java.io.File;
 
+import static com.crypto.Main.*;
 import static com.crypto.model.Mode.*;
+import static com.crypto.util.ImputValidation.*;
+import static com.crypto.util.ImputValidation.validateKey;
 
 public class Run {
     private static EncryptService encryptService = new EncryptService();
     private static DecryptService decryptService = new DecryptService();
     private static BruteForceService bruteForceService = new BruteForceService();
     public static void run (String[] args){
+        validateInput(args);
+        validateCommand(args[COMMAND_ARGUMENT_POSITION]);
+        validateFilePath(args[FILE_PATH_ARGUMENT_POSITION]);
+        validateKey(args[KEY_ARGUMENT_POSITION]);
+
         if (args[0].equals(ENCRYPT.toString())){
             encryptService.encrypt(new File(args[1]), Integer.parseInt(args[2]));
         }
